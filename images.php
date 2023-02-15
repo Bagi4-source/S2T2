7c85c839ff5b3ae3b2b50a626a534240438c0e6e
Bagi4 <67656353+Bagi4-source@users.noreply.github.com>
2023-02-15 16:25:08 +0300

    Add files via upload

--- images.php
+++ images.php
@@ -1,8 +1,19 @@
 <?php
 
+echo '<style type="text/css">
+        html, body {
+            margin: 0;
+            padding: 0;
+        }
+        img {
+            width: 100vw;
+        }
+        </style>';
 
 echo "
-    <video src='images/1.mp4'></video>
+    <video controls>
+        <source src='images/1.mp4' type='video/mp4'>
+    </video>
     <img src='images/2.png'>
     <img src='images/3.png'>
     <img src='images/4.png'>
