1d01b6d1fa0ccd5cf14e730c8f0979635a77b5dc
Bagi4 <67656353+Bagi4-source@users.noreply.github.com>
2023-02-15 16:17:48 +0300

    Add files via upload

---
+++ images.php
@@ -0,0 +1,12 @@
+<?php
+
+
+echo "
+    <video src="images/1.mp4"></video>
+    <img src="images/2.png">
+    <img src="images/3.png">
+    <img src="images/4.png">
+    <img src="images/5.png">
+    <img src="images/6.png">
+    <img src="images/7.png">
+";
